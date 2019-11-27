84460a4f51071e24ece9e841994d404bfc7b9dfd
Julian Losada <Julian_Losada@epam.com>
2019-11-27 12:46:25 +0100

    update serenity version from 2.0.39 to 2.0.52

--- src/test/groovy/com/jonl/example/cucumber/steps/SearchUserStepDefinitions.groovy
+++ src/test/groovy/com/jonl/example/cucumber/steps/SearchUserStepDefinitions.groovy
@@ -1,14 +1,18 @@
 package com.jonl.example.cucumber.steps
 
-import net.serenitybdd.rest.SerenityRest
 import net.serenitybdd.screenplay.Actor
+import net.serenitybdd.screenplay.questions.TheValue
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi
 import net.serenitybdd.screenplay.rest.interactions.Get
+import net.serenitybdd.screenplay.rest.questions.LastResponse
 import org.apache.http.HttpStatus
 
 import static cucumber.api.groovy.EN.*
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat
 import static org.hamcrest.CoreMatchers.equalTo
 import static org.hamcrest.MatcherAssert.assertThat
+
+//import net.serenitybdd.screenplay.questions.ValueOf;
 
 def actor = Actor.named("Julian")
 
@@ -28,26 +32,31 @@ When(~/^An agent searches for him with id (.*)$/) { int userId ->
 }
 
 Then(~/^he should get a virtual account to manage their products$/) { ->
-    assertThat("The response code is the expected",
-            SerenityRest.lastResponse().statusCode(), equalTo(HttpStatus.SC_OK))
-
-    //Search how to use TheValue.of
-    // TheValue.of()
-    /*  actor.should(
-              seeThat(
-                      "el codigo de respuesta", TheValue.of(LastResponse.received().answeredBy(actor).statusCode()),
-                      equalTo(201)
-              )
-      )*/
+    /*  assertThat("The response code is the expected",
+              SerenityRest.lastResponse().statusCode(), equalTo(HttpStatus.SC_OK))*/
+    actor.should(
+            seeThat(
+                    "response code", TheValue.of(LastResponse.received().answeredBy(actor).statusCode()),
+                    equalTo(HttpStatus.SC_CREATED)
+            )
+    )
 }
 
 Then(~/^he should be told that the user doesn't exist$/) { ->
-    assertThat("The response code is the expected",
-            SerenityRest.lastResponse().statusCode(), equalTo(HttpStatus.SC_NOT_FOUND))
+    actor.should(
+            seeThat(
+                    "response code", TheValue.of(LastResponse.received().answeredBy(actor).statusCode()),
+                    equalTo(HttpStatus.SC_NOT_FOUND)
+            )
+    )
 }
 
 Then(~/^he should be told that the user exists$/) { ->
-    assertThat("The response code is the expected",
-            SerenityRest.lastResponse().statusCode(), equalTo(HttpStatus.SC_OK))
+    actor.should(
+            seeThat(
+                    "response code", TheValue.of(LastResponse.received().answeredBy(actor).statusCode()),
+                    equalTo(HttpStatus.SC_OK)
+            )
+    )
 }
 
