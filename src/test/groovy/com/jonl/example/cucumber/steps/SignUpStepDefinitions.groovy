99de0653a3b60645635c6e9f2c67280e28545d05
Julian <jdmesalosada@gmail.com>
2019-11-29 22:19:29 +0100

    added target folder

--- src/test/groovy/com/jonl/example/cucumber/steps/SignUpStepDefinitions.groovy
+++ src/test/groovy/com/jonl/example/cucumber/steps/SignUpStepDefinitions.groovy
@@ -1,48 +1,48 @@
 package com.jonl.example.cucumber.steps
 
-import net.serenitybdd.core.annotations.findby.By
+
 import net.serenitybdd.screenplay.Actor
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb
 import net.serenitybdd.screenplay.actions.Enter
-import net.serenitybdd.screenplay.actions.SendKeys
 import net.serenitybdd.screenplay.actors.OnStage
 import net.serenitybdd.screenplay.actors.OnlineCast
 import net.serenitybdd.screenplay.targets.Target
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 
-import java.util.concurrent.TimeUnit
+import static cucumber.api.groovy.EN.Given
+import static cucumber.api.groovy.EN.Then
+import static cucumber.api.groovy.EN.When
 
-import static cucumber.api.groovy.EN.*
 
 Actor actor
 
 WebDriver driver
 
 
-
 Given(~/^Julian is an user that wants to create a new account$/) {
     ->
 
-    System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/windows/chromedriver.exe");
+    // System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/windows/chromedriver.exe")
+    System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/mac/chromedriver")
     driver = new ChromeDriver()
-
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS)
-    driver.get("https://demoqa.com/keyboard-events-sample-form/");
 
     OnStage.setTheStage(
             OnlineCast.whereEveryoneCan(
-                    BrowseTheWeb.with()
+                    BrowseTheWeb.with(driver)
             )
     )
-    actor = OnStage.theActorCalled("Julian");
+    actor = OnStage.theActorCalled("Julian")
+
+    //driver.manage().timeouts().implicitlyWait(30, TimeUn)
+    driver.get("https://demoqa.com/keyboard-events-sample-form/")
 
 }
 
 When(~/^he sends the required information to sign up$/) {
     ->
 
-    Target USERNAME_FIELD  = Target.the("New Todo Field").locatedBy("#userName")
+    Target USERNAME_FIELD = Target.the("New Todo Field").locatedBy("#userName")
     actor.attemptsTo(
             Enter.theValue("Julian Cho")
                     .into(USERNAME_FIELD),
---
+++ target/generated-sources/groovy-stubs/test/com/jonl/example/cucumber/steps/SearchUserStepDefinitions.java
@@ -0,0 +1,18 @@
+package com.jonl.example.cucumber.steps;
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class SearchUserStepDefinitions
+  extends groovy.lang.Script {
+public SearchUserStepDefinitions
+() {}
+public SearchUserStepDefinitions
+(groovy.lang.Binding context) {}
+public static  void main(java.lang.String... args) { }
+public  java.lang.Object run() { return null;}
+}
