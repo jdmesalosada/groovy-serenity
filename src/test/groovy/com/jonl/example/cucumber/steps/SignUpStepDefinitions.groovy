e7f71dd2ec516a9fb235b07325f6debdbc5fbd89
Julian Losada <Julian_Losada@epam.com>
2019-12-02 14:33:02 +0100

    Added more interactions

--- src/test/groovy/com/jonl/example/cucumber/steps/SignUpStepDefinitions.groovy
+++ src/test/groovy/com/jonl/example/cucumber/steps/SignUpStepDefinitions.groovy
@@ -3,6 +3,7 @@ package com.jonl.example.cucumber.steps
 
 import net.serenitybdd.screenplay.Actor
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb
+import net.serenitybdd.screenplay.actions.Click
 import net.serenitybdd.screenplay.actions.Enter
 import net.serenitybdd.screenplay.actors.OnStage
 import net.serenitybdd.screenplay.actors.OnlineCast
@@ -10,21 +11,22 @@ import net.serenitybdd.screenplay.targets.Target
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 
-import static cucumber.api.groovy.EN.Given
-import static cucumber.api.groovy.EN.Then
-import static cucumber.api.groovy.EN.When
-
+import static cucumber.api.groovy.EN.*
 
 Actor actor
 
 WebDriver driver
 
+Target USERNAME_FIELD = Target.the("Username textbox").locatedBy("#userName")
+Target CURRENT_ADDRESS_FIELD = Target.the("Current address textbox").locatedBy("#currentAddress")
+Target PERMANENT_ADDRESS_FIELD = Target.the("Permanent address textbox").locatedBy("#permanentAddress")
+Target SUBMIT_BUTTON = Target.the("Submit textbox").locatedBy("#submit")
 
 Given(~/^Julian is an user that wants to create a new account$/) {
     ->
 
-    // System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/windows/chromedriver.exe")
-    System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/mac/chromedriver")
+    System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/windows/chromedriver.exe")
+    //System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/mac/chromedriver")
     driver = new ChromeDriver()
 
     OnStage.setTheStage(
@@ -41,11 +43,14 @@ Given(~/^Julian is an user that wants to create a new account$/) {
 
 When(~/^he sends the required information to sign up$/) {
     ->
-
-    Target USERNAME_FIELD = Target.the("New Todo Field").locatedBy("#userName")
     actor.attemptsTo(
             Enter.theValue("Julian Cho")
                     .into(USERNAME_FIELD),
+            Enter.theValue("Francuska 456")
+                    .into(CURRENT_ADDRESS_FIELD),
+            Enter.theValue("Chorzowska 100")
+                    .into(PERMANENT_ADDRESS_FIELD),
+            Click.on(SUBMIT_BUTTON),
     )
 }
 
